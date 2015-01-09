70f4c4306945a54da4a2ec531fff8de2ca52f4b7
wfx <wolfgang.morawetz@gmail.com>
2015-01-09 16:04:14 +0100

    initial (node) teg server

--- client/www/server/server.js
+++ client/www/server/server.js
@@ -1,6 +1,6 @@
-var server=require('node-http-server');
+var server=require( 'node-http-server' );
 
-console.log(server);
+console.log( 'local teg server: 127.0.0.1:8000' );
 
 server.deploy(
     {
