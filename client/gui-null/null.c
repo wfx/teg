e45d3a49635c7fe36c73590cd08809841373cba6
Rüdiger Ranft <rudi@qzzq.de>
2020-10-25 11:36:52 +0100

    Make the null client compilable.
    The null client is needed to be able to link the client code into the test code.

--- client/gui-null/null.c
+++ client/gui-null/null.c
@@ -3,7 +3,7 @@
  * must be done
  */
 
-#include "client.h"
+#include "../../common/common.h"
 
 /* called with at the very beginning, with the originals argc & argv */
 TEG_STATUS gui_init(int argc, char **argv)
@@ -24,91 +24,99 @@ TEG_STATUS gui_exit(char *str)
 }
 
 /* to view messages */
-TEG_STATUS gui_textmsg(char *aString );
+TEG_STATUS gui_textmsg(char *aString )
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_disconnect(void);
+TEG_STATUS gui_disconnect(void)
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_habilitado(int numjug);
+TEG_STATUS gui_habilitado(int numjug)
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_connected();
+TEG_STATUS gui_connected()
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_status(void);
+TEG_STATUS gui_status(void)
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_start(void);
+TEG_STATUS gui_start(void)
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_fichas(int cant, int conts);
+TEG_STATUS gui_fichas(int cant, int conts)
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_turno(void);
+TEG_STATUS gui_turno(void)
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_pais(int p);
+TEG_STATUS gui_pais(int p)
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_pais_select( int pais );
+TEG_STATUS gui_pais_select( int pais )
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_tropas(int src, int dst, int cant);
+TEG_STATUS gui_tropas(int src, int dst, int cant)
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_tarjeta(int pais );
+TEG_STATUS gui_tarjeta(int pais )
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_dados();
+TEG_STATUS gui_dados()
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_reagrupe( int src, int dst, int cant );
+TEG_STATUS gui_reagrupe( int src, int dst, int cant )
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_sensi();
+TEG_STATUS gui_sensi()
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_ataque( int src, int dst );
+TEG_STATUS gui_ataque( int src, int dst )
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_canje();
+TEG_STATUS gui_canje()
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_lost(int numjug);
+TEG_STATUS gui_lost(int numjug)
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_surrender(int numjug);
+TEG_STATUS gui_surrender(int numjug)
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_winner(int numjug, int objetivo);
+TEG_STATUS gui_winner(int numjug, int objetivo)
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_objetivo();
+TEG_STATUS gui_objetivo()
 {
 	return TEG_STATUS_SUCCESS;
 }
-TEG_STATUS gui_textplayermsg(char *n,int nj, char *m );
+TEG_STATUS gui_textplayermsg(char *n,int nj, char *m )
+{
+	return TEG_STATUS_SUCCESS;
+}
+TEG_STATUS gui_country(int country_number)
+{
+	return TEG_STATUS_SUCCESS;
+}
+TEG_STATUS gui_country_select(int country_number)
 {
 	return TEG_STATUS_SUCCESS;
 }
