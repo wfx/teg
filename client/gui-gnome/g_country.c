1530405061231821353a5d4eae695499fef68813
Rüdiger Ranft <teg-git@qzzq.de>
2021-05-06 19:43:25 +0200

    Partial revert "Fixed multiple definitions of global variables"
    This reverts commit e880c3e1de5cbe30575704d4407a360fb69a5445. Unfortunately the
    two variables are named same by incidence, but do not describe the same facts.

--- client/gui-gnome/g_country.c
+++ client/gui-gnome/g_country.c
@@ -47,6 +47,8 @@ struct _G_countries G_countries[COUNTRIES_CANT];
 
 static int initialized = 0;
 static double x, y;
+double new_x, new_y;
+double item_x, item_y;
 static int dragging;
 
 static gboolean enter_notify_cb(GooCanvasItem *item, GooCanvasItem *target,
